aa167cedd63bb9fc9b59065edff90f0be4f52c4b
lukehuang880118 <luke26123720@gmail.com>
2026-08-12 09:50:50 +0800

    add c++ file

---
+++ test/test.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+using namespace std;
+
+int add10(int& i, int& j);
+int& max(int& i, int& j);
+
+int main()
+{
+
+	int a = 5;
+	int b = 2;
+
+	max(a, b) = 1000;
+
+	cout << "a = " << a << "\n";
+	cout << "b = " << b << "\n";
+	return 0;
+}
+
+int add10(int& i, int& j)
+{
+	i = i + 10;
+	j = j + 10;
+
+	return 0;
+}
+
+int& max(int& i, int& j)
+{
+	if (i > j)
+		return i;
+	else
+		return j;
+
+}
