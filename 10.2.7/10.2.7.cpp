839e671bc7ed2942928e084fa43b5a553bd68073
lukehuang880118 <luke26123720@gmail.com>
2026-08-26 18:23:26 +0800

    update

---
+++ 10.2.7/10.2.7.cpp
@@ -0,0 +1,34 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+void find_diff(int a[4][3]);
+
+int main()
+{
+
+	int a[4][3] = { {26,65,37},{10,32,47},{69,76,18} ,{40,74,32} };
+	find_diff(a);
+	
+	return 0;
+}
+
+void find_diff(int a[4][3])
+{
+	//宣告max and min，假設最大最小皆為第一個元素。
+	int max = *(*(a + 0) + 0);
+	int min = *(*(a + 0) + 0);
+	int result = 0;
+
+	int i = 0;
+	while (i < 12)
+	{
+		if (max < *(*a + i))
+			max = *(*a + i);
+		if (min > *(*a + i))
+			min = *(*a + i);
+		i++;
+	}
+
+	result = max - min;
+	cout << "result = " << result << "\n";
+}
---
+++ 10.2.8/10.2.8.cpp
@@ -0,0 +1,16 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+
+#include<iostream>
+#define ROW 2
+#define COL 4
+
+
+
+int main()
+{
+	int arr[ROW][COL] = { {2,3,4,5},{6,7,8,9} };
+
+
+	return 0;
+}
