d8a5b841a6855f34507507338925f0d13ec80a78
lukehuang880118 <luke26123720@gmail.com>
2026-08-26 22:47:40 +0800

    update10.3.9 and 10.3.10

---
+++ chapter10/10.3.10/10.3.10.cpp
@@ -0,0 +1,49 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+#include<iostream>
+
+void string_exchange();
+void print(string* ptr1, string* ptr2);
+
+int main()
+{
+
+	string_exchange();
+
+	return 0;
+}
+
+void string_exchange()
+{
+	//宣告
+	string str1 = "Rome was not built in a day.";
+	string str2 = "Knowledge is power.";
+	string* ptr1 = new string;
+	string* ptr2 = new string;
+
+	
+
+	//指向對應的字串
+	*ptr1 = str1;
+	*ptr2 = str2;
+	
+	cout << str1[2] << "\n";
+
+	print(ptr1, ptr2);
+	
+	(*ptr1).swap(*ptr2);
+	cout << "\n\n";
+
+	print(ptr1, ptr2);
+
+	delete ptr1;
+	ptr1 = nullptr;
+	delete ptr2;
+	ptr2 = nullptr;
+}
+
+void print(string* ptr1, string* ptr2)
+{
+	cout <<"*ptr1 = " << *ptr1 << "\n";
+	cout <<"*ptr2 = " << *ptr2 << "\n";
+}
---
+++ chapter10/10.3.9/10.3.9.cpp
@@ -0,0 +1,30 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+#include<iostream>
+
+void multiply();
+
+int main()
+{
+
+	multiply();
+
+	return 0;
+}
+
+void multiply()
+{
+	int* a = new int[2];
+
+
+	cout << "Input number1:";
+	cin >> *(a + 0);
+	cout << "Input number2:";
+	cin >> *(a + 1);
+
+	cout << "a[0] = " << a[0] << "  a[1] = " << a[1] << "\n";
+	cout << *(a + 0) << " * " << *(a + 1) << " = " << (*(a + 0)) * (*(a + 1));
+
+	delete []a;
+	a = nullptr;
+}
