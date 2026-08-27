6ff0c17a968cceac04ec6fe5efa068eed7805a58
lukehuang880118 <luke26123720@gmail.com>
2026-08-27 12:53:46 +0800

    ch10 finished

---
+++ 10.4.13/10.4.13.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+#include<iostream>
+
+void sort(int& num1, int& num2, int& num3);
+
+int main()
+{
+	int a = 3;
+	int b = 5;
+	int c = 2;
+
+	int& num1 = a;
+	int& num2 = b;
+	int& num3 = c;
+	
+	sort(num1, num2, num3);
+
+
+	return 0;
+}
+
+void sort(int& num1, int& num2, int& num3)//大到小
+{
+	if (num1 < num2)
+		swap(num1, num2);
+
+	if (num2 < num3)
+		swap(num2, num3);
+
+	if (num1 < num2)
+		swap(num1, num2);
+
+
+}
---
+++ 10.4.14/10.4.14.cpp
@@ -0,0 +1,47 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+#include<iostream>
+
+int* min(int* x, int* y);
+int& min_ref(int& x, int& y);
+int* min_ptr(int* x, int* y);
+
+int main()
+{
+	int a = 32;
+	int b = 59;
+	
+
+
+	cout << "a = " << a << ", b = " << b << "\n";
+
+	//min_ref(a, b);				//參照版本呼叫
+	//*min_ptr(&a, &b) = 100;		//指標版本呼叫
+
+	cout << "a = " << a << ", b = " << b << "\n";
+	return 0;
+}
+
+int* min(int* x, int* y)
+{
+	if (x > y)
+		return y;
+	else
+		return x;
+}
+
+int& min_ref(int& x, int& y)
+{
+	if (x > y)
+		return y;
+	else
+		return x;
+}
+
+int* min_ptr(int* x, int* y)
+{
+	if (*x > *y)
+		return y;
+	else
+		return x;
+}
