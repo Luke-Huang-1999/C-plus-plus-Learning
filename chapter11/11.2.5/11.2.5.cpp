f9b7022ed89001645a96fe6d70469a2d8153d673
lukehuang880118 <luke26123720@gmail.com>
2026-09-01 21:57:44 +0800

    update 11.2.5

--- chapter11/11.2.5/11.2.5.cpp
+++ chapter11/11.2.5/11.2.5.cpp
@@ -1,21 +1,32 @@
 ﻿#define _CRT_SECURE_NO_WARNINGS
 using namespace std;
 #include<iostream>
+#define STUDENT_CNTS 3
 
-
-struct student
+typedef struct student
 {
 	string id;
 	string name;
 	int mid_score;
 	int final_score;
 	int regular_score;
-};
+}data[STUDENT_CNTS];
 
+void write_data(student data[STUDENT_CNTS]);
 
 int main()
 {
 
 
 	return 0;
+}
+
+void write_data(student data[STUDENT_CNTS])
+{
+	int i;
+	for (i = 0; i < STUDENT_CNTS; i++)
+	{
+		cout<<
+	}
+
 }
