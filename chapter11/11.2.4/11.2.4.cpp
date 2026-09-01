607d21f947c41850ddf27c5a19bda61a4bbd3cf7
lukehuang880118 <luke26123720@gmail.com>
2026-09-01 11:16:42 +0800

    0

---
+++ chapter11/11.2.4/11.2.4.cpp
@@ -0,0 +1,35 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+#include<iostream>
+
+struct cuboid
+{
+	int length;
+	int width;
+	int height;
+}data;
+
+void volume(cuboid data, int& vol);
+
+int main()
+{
+	cuboid data = { 0 };
+	int vol = 0;
+
+	volume(data, vol);
+
+	cout << "體積 = " << vol << "\n";
+
+	return 0;
+}
+
+void volume(cuboid data, int& vol)
+{
+	int i;
+
+	data.height = 3;
+	data.length = 10;
+	data.width = 2;
+
+	vol = data.height * data.length * data.width;
+}
---
+++ chapter11/11.2.5/11.2.5.cpp
@@ -0,0 +1,21 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+using namespace std;
+#include<iostream>
+
+
+struct student
+{
+	string id;
+	string name;
+	int mid_score;
+	int final_score;
+	int regular_score;
+};
+
+
+int main()
+{
+
+
+	return 0;
+}
