aa0c5f496dd778ca13c24019eae3f6b12b754f98
lukehuang880118 <luke26123720@gmail.com>
2026-09-02 10:27:53 +0800

    update 11.2.6 inside 11.2.5

--- chapter11/11.2.5/11.2.5.cpp
+++ chapter11/11.2.5/11.2.5.cpp
@@ -1,6 +1,7 @@
 ﻿using namespace std;
 #include<iostream>
 #include<string>
+#include <iomanip>
 typedef struct student
 {
 	string id;
@@ -13,6 +14,10 @@ typedef struct student
 
 void write_data1(data1 students[3]);
 void print_data1(data1 students[3]);
+void sort_ID(data1 students[3]);
+void all_print(data1 students[3]);
+void writed_data1(data1 students[3]);
+void sort_score(data1 students[3]);
 
 int main()
 {
@@ -20,12 +25,15 @@ int main()
 	data1 students[3];
 
 	//紀錄資料
-	write_data1(students);
+	//write_data1(students);
+	writed_data1(students);
+	//排序
+	//sort_ID(students);
+	sort_score(students);
+	//列印
+	all_print(students);
 
-	//選擇列印
-	print_data1(students);
 	return 0;
-
 }
 
 void write_data1(data1 students[3])
@@ -66,7 +74,7 @@ void print_data1(data1 students[3])
 	//接收變數
 	cout << "請輸入欲查詢的學生ID：";
 	getline(cin, input);
-	
+
 	//比較並列印
 	int i;
 	for (i = 0; i < 3; i++)
@@ -85,4 +93,81 @@ void print_data1(data1 students[3])
 	}
 
 	cout << "找不到該學生。\n";
+}
+
+void sort_ID(data1 students[3])
+{
+	//宣告
+	data1 tmp;
+
+	//由小到大
+	if (students[0].id > students[1].id)
+	{
+		tmp = students[0];
+		students[0] = students[1];
+		students[1] = tmp;
+	}
+	if (students[1].id > students[2].id)
+	{
+		tmp = students[1];
+		students[1] = students[2];
+		students[2] = tmp;
+	}
+
+	if (students[0].id > students[1].id)
+	{
+		tmp = students[0];
+		students[0] = students[1];
+		students[1] = tmp;
+	}
+}
+
+void all_print(data1 students[3])
+{
+	int i;
+
+	for (i = 0; i < 3; i++)
+	{
+		cout << left << setw(7) << students[i].id << left << setw(15) << students[i].name << setw(4) << students[i].semester_score << "\n";
+	}
+}
+
+void writed_data1(data1 students[3])
+{
+	students[0].id = "95003";
+	students[0].name = "Tippi Lee";
+	students[0].semester_score = 86.7;
+	students[1].id = "95002";
+	students[1].name = "Alice Wu";
+	students[1].semester_score = 85.2;
+	students[2].id = "95001";
+	students[2].name = "Dodu Huang";
+	students[2].semester_score = 92.3;
+}
+
+void sort_score(data1 students[3])
+{
+	//宣告
+	data1 tmp;
+
+	//由小到大
+	if (students[0].semester_score < students[1].semester_score)
+	{
+		tmp = students[0];
+		students[0] = students[1];
+		students[1] = tmp;
+	}
+	if (students[1].semester_score < students[2].semester_score)
+	{
+		tmp = students[1];
+		students[1] = students[2];
+		students[2] = tmp;
+	}
+
+	if (students[0].semester_score < students[1].semester_score)
+	{
+		tmp = students[0];
+		students[0] = students[1];
+		students[1] = tmp;
+	}
 }
