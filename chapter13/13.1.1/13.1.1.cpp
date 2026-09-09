212a37fa357d5c7b84061c84c8020616fd8f1585
lukehuang880118 <luke26123720@gmail.com>
2026-09-09 13:04:22 +0800

    update 13.1

---
+++ chapter13/13.1.1/13.1.1.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class CBodyfit
+{
+private:
+	char id;
+	double height;
+	double weight;
+public:
+	CBodyfit(char i, double h, double w):id(i), height(h), weight(w)
+	{ }
+	void show()
+	{
+		double bmi = weight / ((height / 100) * (height / 100));
+		cout << "學生 " << id << ": 身高 = " << height << "公分, 體重 = " << weight << "公斤, BMI = " << bmi << endl;
+	}
+
+};
+
+int main()
+{
+	CBodyfit stu1('A', 160.5, 45.5);
+	CBodyfit stu2('B', 180.3, 78.2);
+
+	stu1.show();
+	stu2.show();
+
+	return 0;
+}
---
+++ chapter13/13.1.2/13.1.2.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class CWin
+{
+private:
+	char id;
+	int width;
+	int height;
+public:
+	CWin(char i, int w, int h)
+	{
+		id = i;
+		height = h;
+		width = w;
+	}
+
+	CWin()
+	{
+		id = 'B';
+		height = 100;
+		width = 100;
+		cout << "CWin建構子被呼叫了" << endl;
+	}
+	void show_member()
+	{
+		cout << "Window " << id << ":";
+		cout << "width = " << width << ", height = " << height << endl;
+	}
+};
+
+int main()
+{
+	CWin win1('A', 80, 80);
+	CWin win2;
+	win1.show_member();
+	win2.show_member();
+
+	return 0;
+}
