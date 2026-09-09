8d38d4561f5e933167261ccd28f1ea0c4a326e3d
lukehuang880118 <luke26123720@gmail.com>
2026-09-09 09:47:30 +0800

    ch13_9 update

---
+++ chapter13/ch13_9/ch13_9.cpp
@@ -0,0 +1,41 @@
+﻿#include<iostream>
+using namespace std;
+
+class CWin
+{
+private:
+	char id;
+	int width;
+	int height;
+
+public:
+	CWin(char i, int w, int h):id(i), width(w), height(h)
+	{ }
+
+	CWin compare(CWin win)
+	{
+		if (this->area() >= win.area())
+			return *this;
+		else
+			return win;
+	}
+
+	int area()
+	{
+		return (height * width);
+	}
+	char get_id()
+	{
+		return id;
+	}
+};
+int main()
+{
+	CWin win1('A', 70, 80);
+	CWin win2('B', 60, 90);
+
+	cout << "Window " << (win1.compare(win2)).get_id() << " is larger.\n";
+
+
+	return 0;
+}
