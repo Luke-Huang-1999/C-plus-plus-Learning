2c57159068f6224ce47059be6adda29874df92a6
lukehuang880118 <luke26123720@gmail.com>
2026-09-09 13:41:46 +0800

    13.2.3 update

---
+++ chapter13/13.2.3/13.2.3.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class CRectangle
+{
+private:
+	int width;
+	int height;
+public:
+	CRectangle(int w, int h) :width(w), height(h)
+	{ }
+	CRectangle() :width(10), height(10)
+	{ }
+	void show()
+	{
+		cout << "width = " << width << ", height = " << height << endl;
+	}
+};
+
+
+int main()
+{
+	CRectangle cr1(3, 5);
+	cr1.show();
+	CRectangle cr2;
+	cr2.show();
+
+
+	return 0;
+}
