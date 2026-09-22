e813e4802ba86d9c5583ae0da2e77cc78f67fa67
lukehuang880118 <luke26123720@gmail.com>
2026-09-22 13:47:44 +0800

    update 16.1.5

---
+++ chapter16/16.1.5/16.1.5.cpp
@@ -0,0 +1,56 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<iostream>
+using namespace std;
+
+class Caaa
+{
+private:
+	int num1, num2;
+public:
+	void s_num1(int a)
+	{
+		num1 = a;
+	}
+
+	void s_num2(int b)
+	{
+		num2 = b;
+	}
+
+	int get_num1()
+	{
+		return num1;
+	}
+	int get_num2()
+	{
+		return num2;
+	}
+};
+
+class Cbbb:public Caaa
+{
+private:
+public:
+	void set_num(int a, int b)
+	{
+		s_num1(a);
+		s_num2(b);
+	}
+
+	void show()
+	{
+		cout << "num1 = " << get_num1() << ", num2 = " << get_num2() << endl;
+	}
+};
+
+int main()
+{
+	Cbbb obj;
+	
+	obj.set_num(10, 20);
+
+	obj.show();
+
+	
+	return 0;
+}
